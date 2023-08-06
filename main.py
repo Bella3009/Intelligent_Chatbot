d91fed32cc41b28eeacbd12d25da5685d3915a5e
Bella3009 <bellasara13@gmail.com>
2023-08-06 21:50:42 +0200

    Connect backend.py to the main file.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton
 import sys
+from backend import Chatbot
+import threading
 
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.chatbot = Chatbot()
 
         self.setWindowTitle("My Chatbot")
         self.setMinimumSize(620, 500)
@@ -17,12 +21,27 @@ class ChatbotWindow(QMainWindow):
         # Add the input field widget
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 340, 480, 40)
+        self.input_field.returnPressed.connect(self.send_msg)
 
         # Add the button
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_msg)
 
         self.show()
+
+    def send_msg(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#333333'>Me: {user_input}</p>")
+        self.input_field.clear()
+
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input,))
+        thread.start()
+
+    # To show user response immediately in text are and then wait the bot response. Without this responses show together
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333; background-color:#E9E9E9'>Bot: {response}</p>")
 
 
 app = QApplication(sys.argv)
